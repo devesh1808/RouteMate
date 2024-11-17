2353cccf55e24e3b54e9a103af0eca8cddc8f7d0
devesh1808 <devesh1808@gmail.com>
2024-11-18 00:11:28 +0530

    Added Routes and Route in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
+import { Routes, Route } from "react-router-dom";
+import { Home } from "./components/Home.jsx";
+import { ProductList } from "./components/ProductList.jsx";
+import { ProductDetail } from "./components/ProductDetail.jsx";
+import { Contact } from "./components/Contact.jsx";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <header>HEADER</header>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="products" element={<ProductList />}></Route>
+        <Route path="products/1001" element={<ProductDetail />}></Route>
+        <Route path="contact" element={<Contact />}></Route>
+      </Routes>
+      <footer>FOOTER</footer>
     </div>
   );
 }
