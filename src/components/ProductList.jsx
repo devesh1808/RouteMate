2d1115785596ce64e2fed185bdcd2c88acce1cb1
devesh1808 <devesh1808@gmail.com>
2024-11-21 18:11:24 +0530

    used useSearchParams and useLocation

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,3 +1,11 @@
+import { useSearchParams, useLocation } from "react-router-dom";
+
 export const ProductList = () => {
-  return <div className="component">ProductList</div>;
+  const [searchParams] = useSearchParams();
+  const location = useLocation();
+  console.log(location);
+  
+  return (
+    <div className="component">ProductList</div>
+  );
 };
